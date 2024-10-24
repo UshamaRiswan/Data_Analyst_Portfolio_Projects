6f938060843b7b6be80b68ae83d9170fb96dfa35
Ushama Riswan <ushamariswan@gmail.com>
2024-10-24 08:00:03 +0530

    Sql_project

--- 2_Project/Covid_EDA_Project.sql
+++ 2_Project/Covid_EDA_Project.sql
@@ -4,3 +4,8 @@ Covid 19 Data Exploration
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
+
+-- Selecting Data that we are going to start with
+
+SELECT *
+FROM 2_Project.Covid_Dataset
