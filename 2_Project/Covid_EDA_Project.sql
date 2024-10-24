25dbfb6360fa926b56bc6c8b611bca7afa26cccb
Ushama Riswan <ushamariswan@gmail.com>
2024-10-24 08:28:43 +0530

    sql

--- 2_Project/Covid_EDA_Project.sql
+++ 2_Project/Covid_EDA_Project.sql
@@ -8,4 +8,13 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 -- Selecting Data that we are going to start with
 
 SELECT *
-FROM 2_Project.Covid_Dataset
+FROM Data_Analyst_Projects.worldometer_data
+WHERE continent is not NULL
+ORDER BY 3,4
+
+-- Selecting Data that we are going to start with
+
+SELECT location, data, total_cases, new_cases, total_deaths, population
+FROM Data_Analyst_Projects.worldometer
+WHERE location='Jamaica'
+
